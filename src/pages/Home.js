b9e36ff118840ae1598d50f5799fcc69223fb110
srbcheema2 <srbcheema2@gmail.com>
2019-10-25 03:23:20 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,26 +2,26 @@ import React, {Component} from 'react';
 import Footer from '../components/Footer.js'
 import Dropbox from '../components/Dropbox'
 import Loader from '../components/Loader.js'
+
 class Home extends Component{
 	render(){
 		return(
 			<div>	
 				<div className = "container">
 				<h2> Welcome to Exam Scheduler</h2>
-					<p> Paragraph 
-						In literary theory, a text is any object that can be "read", whether this object
-						 is a work of literature, a street sign, an arrangement of 
+					<p> 
+						 Paragraph 
+						 In literary theory, a text is any object that can be "read", whether this object
+						 is a work of literature, a street sign or an arrangement of 
 						 buildings on a city block, or styles of clothing. It is a coherent 
 						 set of signs that transmits some kind of informative message.[1] 
-						 This set of symbols is considered in terms of the informative message's 
+						 This set of symbols is considered in terms of the informative message 
 						 content, rather than in terms of its physical form or the medium in which 
-						 it is represented
+						 it is represented. 
 					 </p>
 					 <div className = "row">
 					 	<div className="col-md-4">
 							 <Dropbox/>
-							 
-
 							 </div>
 							 <div className="col-md-4">
 							 <Dropbox/>
